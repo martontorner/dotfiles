0adb2ff5f49f9ec55336ff746b44ed82f5b5f6c4
Márton Torner <29036669+martontorner@users.noreply.github.com>
2026-02-27 13:17:37 +0100

    feat: automatically attach/create tmux in interactive shells

--- bash/bash_profile.bash
+++ bash/bash_profile.bash
@@ -8,3 +8,6 @@ _source_if_exists "${DOTFILES}/shell/_/banners.sh"
 
 # Interactive
 _source_if_exists "${HOME}/.bashrc"
+if [[ $- == *i* ]] && [ -z "$TMUX" ] && command -v tmux >/dev/null 2>&1; then
+    tmux attach 2>/dev/null || tmux new
+fi
--- zsh/zprofile.zsh
+++ zsh/zprofile.zsh
@@ -8,3 +8,6 @@ _source_if_exists "${DOTFILES}/shell/_/banners.sh"
 
 # Interactive
 # zsh automatically sources .zshrc for interactive shells
+if [[ $- == *i* ]] && [ -z "$TMUX" ] && command -v tmux >/dev/null 2>&1; then
+    tmux attach 2>/dev/null || tmux new
+fi
